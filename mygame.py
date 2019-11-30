1f6648ceecbe506407d4e0f85af3714089350c07
jotsio <jussi.t.huhtala@gmail.com>
2019-11-30 14:15:28 +0200

    Add simple keyboad controls

--- mygame.py
+++ mygame.py
@@ -2,24 +2,34 @@ import sys, pygame
 pygame.init()
 
 size = width, height = 800, 600
-speed = [1, 1]
+speed = [0, 0]
+startPos = [400, 500]
 black = 0, 0, 0
+
 
 screen = pygame.display.set_mode(size)
 
-ball = pygame.image.load("assets/ship_default.png")
-ballrect = ball.get_rect()
+ship = pygame.image.load("assets/ship_default.png")
+shipRect = ship.get_rect()
+shipRect = shipRect.move(startPos)
 
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
-
-    ballrect = ballrect.move(speed)
-    if ballrect.left < 0 or ballrect.right > width:
-        speed[0] = -speed[0]
-    if ballrect.top < 0 or ballrect.bottom > height:
-        speed[1] = -speed[1]
+    
+        shipRect = shipRect.move(speed)
+        # checks keypresses
+        pressed = pygame.key.get_pressed()
+        if pressed[pygame.K_LEFT]:
+            speed[0] = -2
+        if pressed[pygame.K_RIGHT]:
+            speed[0] = 2
+        # bounces from the wall
+        if shipRect.left < 0 or shipRect.right > width:
+           speed[0] = -speed[0]
+        if shipRect.top < 0 or shipRect.bottom > height:
+           speed[1] = -speed[1]
 
     screen.fill(black)
-    screen.blit(ball, ballrect)
+    screen.blit(ship, shipRect)
     pygame.display.flip()
