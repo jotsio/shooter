fbde7ac34764a33b658b3ce1941e5a7ae959bead
jotsio <jussi.t.huhtala@gmail.com>
2019-12-04 22:34:07 +0200

    Make more complex movement

--- mygame.py
+++ mygame.py
@@ -21,46 +21,55 @@ class Particle:
 class PlayerShip:
     def __init__(self, x, y):
         self.startPos = [x, y]
-        self.speed = [0, 0]  
+        self.speed = [0.000, 0.000]  
         self.img = pygame.image.load("assets/ship_default.png")
         self.rect = self.img.get_rect()
         self.rect = self.rect.move(x, y)
         print("Player is ready")  
     def moveX(self):
         self.rect = self.rect.move(self.speed[0], 0 )
+        if self.speed[0] > 0.000 :
+            self.speed[0] = self.speed[0] - 0.001
+        if self.speed[0] < 0.000 :
+            self.speed[0] = self.speed[0] + 0.001
+    def setThrustX(self, amount):
+ #       if self.speed[0] <= 1 and self.speed[0] >= -1:
+            self.speed[0] = self.speed[0] + amount
     def setSpeed(self, amount):
         self.speed[0] = amount
     def bounceX(self):
         self.speed[0] = -self.speed[0]
     def bounceY(self):
         self.speed[1] = -self.speed[1]
-    
 
-#pelaajan luonti
-player = PlayerShip(60,70)
-print (player.rect)
+
+#define screen
 
 size = width, height = 800, 600
 black = 0, 0, 0
+screen = pygame.display.set_mode(size)    
 
-#ruudun maarittely
-screen = pygame.display.set_mode(size)
+# Create player
+player = PlayerShip(400,500)
+print (player.rect)
+
 
 while 1:
+    # Keyevents listener
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
-        # checks keypresses
         pressed = pygame.key.get_pressed()
-        if pressed[pygame.K_LEFT]:
-            player.setSpeed(-4)
-        if pressed[pygame.K_RIGHT]:
-            player.setSpeed(4)
-        # bounces from the wall
-        if player.rect.left < 0 or player.rect.right > width:
-           player.bounceX()
-        if player.rect.top < 0 or player.rect.bottom > height:
-           player.bounceY()
-        player.moveX()
+        player.setThrustX(pressed[pygame.K_RIGHT]-pressed[pygame.K_LEFT])
+    
+    # Player movement
+    player.moveX()
+
+    # bounces from the wall
+    if player.rect.left < 0 or player.rect.right > width:
+        player.bounceX()
+    if player.rect.top < 0 or player.rect.bottom > height:
+        player.bounceY()
+    
 
     screen.fill(black)
     screen.blit(player.img, player.rect)
